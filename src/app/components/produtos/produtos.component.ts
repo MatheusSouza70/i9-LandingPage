445d2391a299bdff6eda07354f2e35d7ac98698a
Matheus Dutra Souza <88992046+MatheusSouza70@users.noreply.github.com>
2024-09-10 11:51:09 -0300

    feat: added products and about pages design

--- src/app/components/produtos/produtos.component.ts
+++ src/app/components/produtos/produtos.component.ts
@@ -1,37 +1,72 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { HeaderComponent } from "../header/header.component";
+import { CommonModule } from '@angular/common';
+import { NgOptimizedImage } from '@angular/common';
+import { FooterComponent } from '../footer/footer.component';
+
+interface Produto {
+  nome: string;
+  imagem: string;
+  precoOriginal: number;
+  precoAtual: number;
+  desconto: string;
+  categoria: string[];
+}
+
 
 @Component({
   selector: 'app-produtos',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, CommonModule,FooterComponent, NgOptimizedImage],
   templateUrl: './produtos.component.html',
   styleUrl: './produtos.component.scss'
 })
-export class ProdutosComponent implements OnInit {
-  produtos = [
+export class ProdutosComponent {
+  produtos: Produto[] = [
     {
-      nome: 'Produto 1',
-      descricao: 'Descrição do produto 1',
-      preco: 99.99,
-      imagem: 'assets/produto1.jpg' // Caminho para a imagem do produto
+      nome: 'AccuDrawer™',
+      imagem: '/assets/accudrawer.png',
+      precoOriginal: 2000.0,
+      precoAtual: 1900.0,
+      desconto: '5%',
+      categoria: ['hardware', 'drawer'],
     },
     {
-      nome: 'Produto 2',
-      descricao: 'Descrição do produto 2',
-      preco: 149.99,
-      imagem: 'assets/produto2.jpg' // Caminho para a imagem do produto
+      nome: 'AccuCab',
+      imagem: '/assets/accucab.png',
+      precoOriginal: 3500.0,
+      precoAtual: 3400.0,
+      desconto: '3%',
+      categoria: ['hardware', 'cabinet'],
     },
     {
-      nome: 'Produto 3',
-      descricao: 'Descrição do produto 3',
-      preco: 79.99,
-      imagem: 'assets/produto3.jpg' // Caminho para a imagem do produto
-    }
+      nome: 'Express Locker',
+      imagem: '/assets/express-locker.png',
+      precoOriginal: 2500.0,
+      precoAtual: 2400.0,
+      desconto: '4%',
+      categoria: ['hardware', 'locker', 'express'],
+    },
+    {
+      nome: 'ProLock',
+      imagem: '/assets/prolock.png',
+      precoOriginal: 4000.0,
+      precoAtual: 3800.0,
+      desconto: '5%',
+      categoria: ['hardware', 'security'],
+    },
   ];
+  produtosFiltrados: Produto[] = [...this.produtos];
 
-  constructor() { }
-
-  ngOnInit(): void {
+  // Função para filtrar produtos com base na categoria selecionada
+  filtrarProdutos(categoria: string): void {
+    // Se categoria for 'all' ou vazio, mostramos todos os produtos
+    if (categoria === 'all' || !categoria) {
+      this.produtosFiltrados = [...this.produtos];
+    } else {
+      this.produtosFiltrados = this.produtos.filter((produto) =>
+        produto.categoria.includes(categoria)
+      );
+    }
   }
 }
--- src/app/components/sobre/sobre.component.ts
+++ src/app/components/sobre/sobre.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../header/header.component";
+import { FooterComponent } from "../footer/footer.component";
 
 @Component({
   selector: 'app-sobre',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, FooterComponent,FooterComponent],
   templateUrl: './sobre.component.html',
   styleUrl: './sobre.component.scss'
 })
